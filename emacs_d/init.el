b083b727e5ded140154e6d12cd6b343c59313f1a
Jon Rogers <jon@carbonfive.com>
2019-09-27 08:11:20 -0700

    Add projectile config that defines hot key

--- emacs_d/init.el
+++ emacs_d/init.el
@@ -19,6 +19,7 @@
 (load (format "%s/%s" emacs-configuration-file-directory "ruby.el"))
 (load (format "%s/%s" emacs-configuration-file-directory "typescript.el"))
 (load (format "%s/%s" emacs-configuration-file-directory "custom.el"))
+(load (format "%s/%s" emacs-configuration-file-directory "projectile.el"))
 
 (package-initialize)
 (unless package-archive-contents
@@ -43,3 +44,27 @@
           (lambda (frame)
             (projectile-mode)
             ))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
+ '(grep-find-ignored-directories
+   (quote
+    ("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "log" ".meteor" "dist" "node_modules" "*/node_modules" ".idea")))
+ '(ido-enable-flex-matching t)
+ '(ido-use-faces nil)
+ '(inhibit-startup-screen t)
+ '(javascript-indent-level 2 t)
+ '(jsx-indent-level 2 t)
+ '(package-selected-packages
+   (quote
+    (projectile projectile-codesearch projectile-direnv projectile-git-autofetch projectile-rails tide rjsx-mode emojify web-mode tramp-term sws-mode slim-mode scss-mode sass-mode ruby-tools ruby-compilation rubocop multiple-cursors magit jump jsx-mode json-mode js2-mode jade-mode icicles git-blame flycheck flx-ido feature-mode exec-path-from-shell color-theme-sanityinc-solarized color-theme coffee-mode base16-theme alchemist ag add-node-modules-path)))
+ '(standard-indent 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
